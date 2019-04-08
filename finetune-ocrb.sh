755b9b0ce9b4ab81b7890b71c3dcab468b0c10dd
Shreeshrii <shreeshrii@gmail.com>
2019-04-09 01:18:02 +0530

    plus finetune, 600 iterations with training text of 150 lines

--- finetune-ocrb.sh
+++ finetune-ocrb.sh
@@ -6,6 +6,8 @@ time ~/tesseract/src/training/tesstrain.sh \
   --noextract_font_properties \
   --langdata_dir ~/langdata \
   --tessdata_dir ~/tessdata_best \
+  --exposures "-1 0 1" \
+  --save_box_tiff \
   --fontlist "OCR B MT" "OCR-B 10 BT" \
   --training_text ./eng.MRZ.training_text \
   --workspace_dir ~/tmp \
@@ -15,38 +17,45 @@ time ~/tesseract/src/training/tesstrain.sh \
 
 echo "/n ****** Finetune one of the fully-trained existing models: ***********"
 
-mkdir -p ~/tesstutorial/ocrb_from_full
+rm -rf  ~/tesstutorial/ocrb_from_full
+mkdir  ~/tesstutorial/ocrb_from_full
 
 combine_tessdata -e ~/tessdata_best/eng.traineddata \
   ~/tesstutorial/ocrb_from_full/eng.lstm
   
-OMP_THREAD_LIMIT=1  lstmtraining \
+lstmtraining \
   --model_output ~/tesstutorial/ocrb_from_full/ocrb_plus \
   --traineddata ~/tesstutorial/ocrb/eng/eng.traineddata \
   --continue_from ~/tesstutorial/ocrb_from_full/eng.lstm \
   --old_traineddata ~/tessdata_best/eng.traineddata \
   --train_listfile ~/tesstutorial/ocrb/eng.training_files.txt \
   --debug_interval -1 \
-  --max_iterations 410
+  --max_iterations 600
   
   lstmtraining \
 --stop_training \
   --traineddata ~/tesstutorial/ocrb/eng/eng.traineddata \
   --continue_from ~/tesstutorial/ocrb_from_full/ocrb_plus_checkpoint \
-  --model_output ocrb_plus.traineddata
+  --model_output ~/tesstutorial/ocrb_from_full/ocrb.traineddata
   
-OMP_THREAD_LIMIT=1 lstmeval \
-  --model ~/tesstutorial/ocrb_from_full/ocrb_plus_checkpoint \
+  lstmtraining \
+--stop_training \
+  --convert_to_int \
   --traineddata ~/tesstutorial/ocrb/eng/eng.traineddata \
+  --continue_from ~/tesstutorial/ocrb_from_full/ocrb_plus_checkpoint \
+  --model_output ~/tesstutorial/ocrb_from_full/ocrb_int.traineddata
+  
+OMP_THREAD_LIMIT=1 time lstmeval \
+  --model ~/tesstutorial/ocrb_from_full/ocrb.traineddata \
   --eval_listfile ~/tesstutorial/ocrb/eng.training_files.txt
   
-OMP_THREAD_LIMIT=1 lstmeval \
+OMP_THREAD_LIMIT=1 time lstmeval \
+  --model ~/tesstutorial/ocrb_from_full/ocrb_int.traineddata \
+  --eval_listfile ~/tesstutorial/ocrb/eng.training_files.txt
+  
+OMP_THREAD_LIMIT=1 time lstmeval \
   --verbosity 0 \
   --model ~/tessdata_best/eng.traineddata \
   --eval_listfile ~/tesstutorial/ocrb/eng.training_files.txt
 
-OMP_THREAD_LIMIT=1 lstmeval \
-  --verbosity 0 \
-  --model ~/tessdata_best/script/Latin.traineddata \
-  --eval_listfile ~/tesstutorial/ocrb/eng.training_files.txt
 
